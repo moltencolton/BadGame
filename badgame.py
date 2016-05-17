914e385f43f2a95e8706f15ee022ff91716b1444
dreadpirate223345 <dread@localhost.localdomain>
2016-05-17 14:28:36 -0700

    This is the new code, added images for diffrent rooms, stairs for changing rooms, and fixed movement.

--- badgame.py
+++ badgame.py
@@ -16,8 +16,6 @@ FLOOR_HIGHT = 500
 FLOOR_WITH  = 700
 FLOOR_LIST = [50,50,700,500]
 
-
-
 class Wall_Art_To_Sprite(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
@@ -27,10 +25,19 @@ class Wall_Art_To_Sprite(pygame.sprite.Sprite):
 '''	this class is the process that takes an image passed in, and converts
  	it into a sprite that is better used in the game. line 32 uses this to fill the gameDislpay
  	with the background color behind all other objects that we add '''
-
-def drawRoom():
+ 	
+def roomChange(lead_x, lead_y, current_Room):
+	if current_Room == Room1_Wall:
+		if lead_x >= 450 and lead_x <= 510 and lead_y >= 450 and lead_y <= 510:
+			return Room2_Wall	
+	if current_Room == Room2_Wall:
+		if lead_x >= 150 and lead_x <= 210 and lead_y >= 150 and lead_y <= 210:
+			return Room1_Wall
+	return current_Room
+	
+def drawRoom(WallArt_1):
 	gameDisplay.fill(white)
-	gameDisplay.blit(WallArt.image, WallArt.rect)
+	gameDisplay.blit(WallArt_1.image, WallArt_1.rect)
 	pygame.draw.rect(gameDisplay, tan, FLOOR_LIST)
 
 def drawLead(lead_x, lead_y):
@@ -39,6 +46,13 @@ def drawLead(lead_x, lead_y):
 	pygame.draw.rect(gameDisplay, red, [lead_x + 10 ,lead_y + 40,20,20 ])
 	pygame.draw.rect(gameDisplay, red, [lead_x - 20 ,lead_y + 40,20,20 ]) 
 	
+def drawStairs(current_Room):
+	if current_Room == Room1_Wall:
+		pygame.draw.rect(gameDisplay, black, [450,450,60,60])
+	if current_Room == Room2_Wall:
+		pygame.draw.rect(gameDisplay, black, [150,150,60,60])
+
+
 def checkCollision(lead_x,lead_x_change, lead_y,lead_y_change):
 	lead_x += lead_x_change
 	lead_y += lead_y_change
@@ -50,12 +64,13 @@ gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('pygamegame')
 
 
-
 gameExit = False
 
-WallArt = Wall_Art_To_Sprite('background_image.jpg', [0,0])
+Room1_Wall = Wall_Art_To_Sprite('background_image.jpg', [0,0])
 '''	This is just a test image that is passed to the class Background before real
 	art can be made for the walls and such. more specific names need to be used'''
+Room2_Wall = Wall_Art_To_Sprite('green_crazy_circle-800x600.jpg', [0,0])
+
 
 lead_x = 300
 lead_y = 300
@@ -65,12 +80,14 @@ movLeft = False
 movRight = False
 movUp = False
 movDown = False
+collide = False	
 
 clock = pygame.time.Clock()
+current_Room = Room1_Wall
 
 while not gameExit:
-	drawRoom()
-	
+	drawRoom(current_Room)
+	drawStairs(current_Room)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
@@ -113,8 +130,10 @@ while not gameExit:
 				if movUp == True:
 					lead_y_change = -WALKING_SPEED
 		
-	collide = False		
+		
 	collide = checkCollision(lead_x,lead_x_change, lead_y,lead_y_change)
+	current_Room = roomChange(lead_x,lead_y, current_Room)
+	
 	if collide == False:
 		lead_x += lead_x_change
 		lead_y += lead_y_change
