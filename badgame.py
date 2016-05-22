4c78aafa6a2ebbe2c25c250970e518ac867739bb
Dread <aaron.statt@wsu.edu>
2016-05-21 21:02:32 -0700

    Updated 5-21-15 try two

--- badgame.py
+++ badgame.py
@@ -1,7 +1,17 @@
-#Running code in linux comandline: python2 badgame.py
-''' 1: git add badgame.py  
-    2: git commit -m "message" 
-    3: git push origin master'''
+'''
+	Running code in linux comandline: python2 badgame.py
+	1: git add badgame.py  
+	2: git commit -m "message" 
+	3: git push origin master
+'''
+'''
+	MAIN NOTES! LIST OF THINGS THAT NEED TO BE DONE
+	1: Code needs to start to be broken up into diffrent files for organization
+	2: Fixing how the class is written. the class is not created properly to 
+	   store all the information it does
+	3: Create real sprites for lead charicter and floor/objects
+'''
+
 import pygame
 
 pygame.init()
@@ -11,66 +21,97 @@ black = (0  ,0  ,0  )
 red   = (255,0  ,0  )
 tan   = (234,197,146)
 
-WALKING_SPEED = 10
-FLOOR_HIGHT = 500
-FLOOR_WITH  = 700
 FLOOR_LIST = [50,50,700,500]
+CHAR_BUFFER = 5
+WALL_BUFFER = FLOOR_LIST[0]
+WALKING_SPEED = 10
+FLOOR_HIGHT = FLOOR_LIST[3]
+FLOOR_WITH  = FLOOR_LIST[2]
 
-class Wall_Art_To_Sprite(pygame.sprite.Sprite):
-    def __init__(self, image_file, location):
+
+class Create_Room_Sprite(pygame.sprite.Sprite):
+    def __init__(self, image_file, location, doorPosition, link):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-'''	this class is the process that takes an image passed in, and converts
- 	it into a sprite that is better used in the game. line 32 uses this to fill the gameDislpay
- 	with the background color behind all other objects that we add '''
- 	
-def roomChange(lead_x, lead_y, current_Room):
-	if current_Room == Room1_Wall:
-		if lead_x >= 450 and lead_x <= 510 and lead_y >= 450 and lead_y <= 510:
-			return Room2_Wall	
-	if current_Room == Room2_Wall:
-		if lead_x >= 150 and lead_x <= 210 and lead_y >= 150 and lead_y <= 210:
-			return Room1_Wall
-	return current_Room
-	
-def drawRoom(WallArt_1):
-	gameDisplay.fill(white)
-	gameDisplay.blit(WallArt_1.image, WallArt_1.rect)
-	pygame.draw.rect(gameDisplay, tan, FLOOR_LIST)
+        self.door = doorPosition
+        if link != False:
+        	self.doorLink = link
+'''
+	this class was origanlly used just for creating and returning the room
+	image after being correctly processed how java wants it.
+	although this also became a great place to store elements of each room
+	almost like a struct. having diffrent quordanates for each door position
+	and what room that door links to. 
+		this class needs to be fixed to be much more elignet.
+	Syntax:
+	object.image returns image used for walls of room
+	object.door returns the list of [x,y,with,hight] of the door (pos and size)
+	object.doorLink returns what object (room) the stairs link to 
+'''
 
-def drawLead(lead_x, lead_y):
+def roomChange(lead_x, lead_y, current_Room):
+	if lead_x >= current_Room.door[0] and lead_x <= (current_Room.door[0] + current_Room.door[2]) and lead_y >= current_Room.door[1] and lead_y <= (current_Room.door[1] + current_Room.door[2]):
+		return current_Room.doorLink
+	return current_Room
+'''
+	This function checks to see if the player is touching the stairs, 
+	if the player is within the boundary of the stairs, it returns the 
+	room that that stair leads too, if not, it returns the same room as
+	was passed in
+'''
 	
+def drawRoom(current_Room):
+	gameDisplay.fill(white)
+	gameDisplay.blit(current_Room.image, current_Room.rect)
+	pygame.draw.rect(gameDisplay, tan, FLOOR_LIST)
+	pygame.draw.rect(gameDisplay, black, current_Room.door)
+'''
+	Does not need much explanation, function draws the room for 
+	the "current_Room" that is passed into it. unsing calls to the class
+'''
+def drawLead(lead_x, lead_y):
+	pygame.draw.circle(gameDisplay,red, [lead_x, lead_y] , CHAR_BUFFER * 2, 0)
+	
+'''	
+	OLD SPRITE, I drew a new one quickly so I could show this in public
 	pygame.draw.rect(gameDisplay, red, [lead_x,lead_y,10, 40]) #(x,y,with,hight)
 	pygame.draw.rect(gameDisplay, red, [lead_x + 10 ,lead_y + 40,20,20 ])
 	pygame.draw.rect(gameDisplay, red, [lead_x - 20 ,lead_y + 40,20,20 ]) 
 	
-def drawStairs(current_Room):
-	if current_Room == Room1_Wall:
-		pygame.draw.rect(gameDisplay, black, [450,450,60,60])
-	if current_Room == Room2_Wall:
-		pygame.draw.rect(gameDisplay, black, [150,150,60,60])
+'''
 
-
-def checkCollision(lead_x,lead_x_change, lead_y,lead_y_change):
+def wallCollision(lead_x,lead_x_change, lead_y,lead_y_change):
 	lead_x += lead_x_change
 	lead_y += lead_y_change
-	if lead_x >= 750 - 20 or lead_x <= 60 or lead_y >= 550 - 50 or lead_y <= 40:
+	if lead_x >= current_Room.door[0] and lead_x <= (current_Room.door[0] + current_Room.door[2]) and lead_y >= current_Room.door[1] and lead_y <= (current_Room.door[1] + current_Room.door[2]):
+		return False
+	if lead_x >= (FLOOR_WITH + WALL_BUFFER - CHAR_BUFFER) or lead_x <= (WALL_BUFFER + CHAR_BUFFER): 
+		return True
+	if lead_y >= (FLOOR_HIGHT + WALL_BUFFER - CHAR_BUFFER) or lead_y <= (WALL_BUFFER + CHAR_BUFFER):
 		return True
 	return False
+	
+'''
+	Has not changed, except for changing numbers for Global variables
+'''
  
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('pygamegame')
 
-
 gameExit = False
 
-Room1_Wall = Wall_Art_To_Sprite('background_image.jpg', [0,0])
-'''	This is just a test image that is passed to the class Background before real
-	art can be made for the walls and such. more specific names need to be used'''
-Room2_Wall = Wall_Art_To_Sprite('green_crazy_circle-800x600.jpg', [0,0])
+RoomEnd    = Create_Room_Sprite('wall16-800.jpg',[0,0], [0,0,0,0], False)
+Room2_Wall = Create_Room_Sprite('green_crazy_circle-800x600.jpg', [0,0], [150,150,60,60], RoomEnd)
+Room1_Wall = Create_Room_Sprite('background_image.jpg', [0,0], [0,450,50,60], Room2_Wall)
 
+'''	This is just a test image that is passed to the class
+	Background before real art can be made for the walls and such.
+	more specific names need to be used
+	syntax:
+		Create_Room_Sprite('link to image' [0,0], [x,y,with,hight] for stairs, what room stairs lead)
+'''
 
 lead_x = 300
 lead_y = 300
@@ -83,11 +124,10 @@ movDown = False
 collide = False	
 
 clock = pygame.time.Clock()
-current_Room = Room1_Wall
+current_Room = Room1_Wall 
 
 while not gameExit:
 	drawRoom(current_Room)
-	drawStairs(current_Room)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
@@ -131,7 +171,7 @@ while not gameExit:
 					lead_y_change = -WALKING_SPEED
 		
 		
-	collide = checkCollision(lead_x,lead_x_change, lead_y,lead_y_change)
+	collide = wallCollision(lead_x,lead_x_change, lead_y,lead_y_change)
 	current_Room = roomChange(lead_x,lead_y, current_Room)
 	
 	if collide == False:
