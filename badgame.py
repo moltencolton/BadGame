ee68c249fcbf92fa55bda2e2a99afdca4ed28de4
moltencolton <colton.chambers@colorado.edu>
2016-05-22 17:30:00 -0600

    started adding enemies but im not sure how

--- badgame.py
+++ badgame.py
@@ -2,18 +2,18 @@
 	Written by Aaron Statt and Colton Chambers
 
 	Running code in linux comandline: python2 badgame.py
-	1: git add badgame.py  
-	2: git commit -m "message" 
+	1: git add badgame.py
+	2: git commit -m "message"
 	3: git push origin master
 '''
 '''
 	MAIN NOTES! LIST OF THINGS THAT NEED TO BE DONE
 	1: Code needs to start to be broken up into diffrent files for organization
-	2: Create a way to have multipal doors in the same room linking to diffrent
+	2: Create a way to have multipul doors in the same room linking to different
 	   places
 	3: Create real sprites for lead charicter and floor/objects
 	4: Creating a goal for the game, items, enamies, and other objects
-	
+
 '''
 
 import pygame
@@ -41,9 +41,9 @@ class Lead:
 '''
 	This is the Class that had to be created for Lead, instead
 	of pushing around his location to every function, this lets
-	him be an object that can be changed by methods globaly and 
+	him be an object that can be changed by methods globaly and
 	let us expand the data associated with him esially
-'''	
+'''
 
 
 class Create_Room_Sprite(pygame.sprite.Sprite):
@@ -54,24 +54,29 @@ class Create_Room_Sprite(pygame.sprite.Sprite):
 	        self.rect.left, self.rect.top = location
 		self.doors = []
 		self.links = []
+		self.enemies = []
 	def addStairs(self, doorPositions, link):
 		if link != False:
 			self.doors.append(doorPositions)
 			self.links.append(link)
-		
+    def addEnemy(self, doorPositions, link):
+         while link != False:
+			self.doors.append(enemyPositions)
+			self.links.append(link)
+
 '''
 	this class was origanlly used just for creating and returning the room
 	image after being correctly processed how java wants it.
 	although this also became a great place to store elements of each room
 	almost like a struct. having diffrent quordanates for each door position
-	and what room that door links to. 
-	
-	there is a function in this class that adds a new set of stairs to the 
+	and what room that door links to.
+
+	there is a function in this class that adds a new set of stairs to the
 	room class, this is not working however and needs to be addressed,
 	the function is called by currnetRoom.addStairs([array of door pos], room link)
 	the array or doors pos is given in [x,y,whith,hight]
 	len()
-	
+
 '''
 
 def roomChange(lead, current_Room):
@@ -82,14 +87,30 @@ def roomChange(lead, current_Room):
 			return current_Room.links[i]
 	return current_Room
 '''
-	This function checks to see if the player is touching the stairs, 
-	if the player is within the boundary of the stairs, it returns the 
+	This function checks to see if the player is touching the stairs,
+	if the player is within the boundary of the stairs, it returns the
 	room that that stair leads too, if not, it returns the same room as
 	was passed in.
-	This for loop then cicles through all of the stairs making sure that 
+	This for loop then cicles through all of the stairs making sure that
 	the player can use all of them.
 '''
-	
+
+def enemyCollision(lead, current_Room):
+	for i in range(0, len(current_Room.doors)):
+		if lead.x >= current_Room.enemies[i][0] and lead.x <= (current_Room.enemies[i][0] + current_Room.enemies[i][2]) and lead.y >= current_Room.enemies[i][1] and lead.y <= (current_Room.enemies[i][1] + current_Room.enemies[i][2]):
+			lead.x = 300
+			lead.y = 300
+			return current_Room
+	return current_Room
+'''
+	This function checks to see if the player is touching the stairs,
+	if the player is within the boundary of the stairs, it returns the
+	room that that stair leads too, if not, it returns the same room as
+	was passed in.
+	This for loop then cicles through all of the stairs making sure that
+	the player can use all of them.
+'''
+
 def drawRoom(current_Room):
 	gameDisplay.fill(white)
 	gameDisplay.blit(current_Room.image, current_Room.rect)
@@ -97,18 +118,18 @@ def drawRoom(current_Room):
 	for i in range(0, len(current_Room.doors)):
 		pygame.draw.rect(gameDisplay, black, current_Room.doors[i])
 '''
-	Does not need much explanation, function draws the room for 
+	Does not need much explanation, function draws the room for
 	the "current_Room" that is passed into it. unsing calls to the class
 '''
 def drawLead(lead):
 	pygame.draw.circle(gameDisplay,red, [lead.x, lead.y] , CHAR_BUFFER * 2, 0)
-	
-'''	
+
+'''
 	OLD SPRITE, I drew a new one quickly so I could show this in public
 	pygame.draw.rect(gameDisplay, red, [lead.x,lead.y,10, 40]) #(x,y,with,hight)
 	pygame.draw.rect(gameDisplay, red, [lead.x + 10 ,lead.y + 40,20,20 ])
-	pygame.draw.rect(gameDisplay, red, [lead.x - 20 ,lead.y + 40,20,20 ]) 
-	
+	pygame.draw.rect(gameDisplay, red, [lead.x - 20 ,lead.y + 40,20,20 ])
+
 '''
 
 def wallCollision(lead):
@@ -119,21 +140,21 @@ def wallCollision(lead):
 			#lead.x += lead.x_change
 			#lead.y += lead.y_change
 			return False
-	if lead.x_predict >= (FLOOR_WITH + WALL_BUFFER - CHAR_BUFFER) or lead.x_predict <= (WALL_BUFFER + CHAR_BUFFER): 
+	if lead.x_predict >= (FLOOR_WITH + WALL_BUFFER - CHAR_BUFFER) or lead.x_predict <= (WALL_BUFFER + CHAR_BUFFER):
 		return True
 	if lead.y_predict >= (FLOOR_HIGHT + WALL_BUFFER - CHAR_BUFFER) or lead.y_predict <= (WALL_BUFFER + CHAR_BUFFER):
 		return True
 	return False
-	
+
 '''
-	This check wall function was our check colision function, but had to be 
+	This check wall function was our check colision function, but had to be
 	changed to adapt to exempt hitting the wall if a door was placed on that
 	wall, also a lead.x and y predicted was added to look ahead a frame without
-	adding lead_change to the movement yet. 
+	adding lead_change to the movement yet.
 	the for loop is added to check all of the doors in the room.
 '''
- 
-gameDisplay = pygame.display.set_mode((800,600)) 
+
+gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('pygamegame')
 
 gameExit = False
@@ -159,10 +180,10 @@ movLeft = False
 movRight = False
 movUp = False
 movDown = False
-collide = False	
+collide = False
 
 clock = pygame.time.Clock()
-current_Room = Room1_Wall 
+current_Room = Room1_Wall
 
 while not gameExit:
 	drawRoom(current_Room)
@@ -181,45 +202,45 @@ while not gameExit:
 				lead.y_change =  -WALKING_SPEED
 			if event.key == pygame.K_DOWN:
 				movDown = True
-				lead.y_change = WALKING_SPEED  
-		
+				lead.y_change = WALKING_SPEED
+
 		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT:  
+			if event.key == pygame.K_LEFT:
 				lead.x_change = 0
 				movLeft = False
 				if movRight == True:
 					lead.x_change = WALKING_SPEED
-					
+
 			if event.key == pygame.K_RIGHT:
 				lead.x_change = 0
 				movRight = False
 				if movLeft == True:
 					lead.x_change = -WALKING_SPEED
-			
-			if event.key == pygame.K_UP:  
+
+			if event.key == pygame.K_UP:
 				lead.y_change = 0
 				movUp = False
 				if movDown == True:
 					lead.y_change = WALKING_SPEED
-					
+
 			if event.key == pygame.K_DOWN:
 				lead.y_change = 0
 				movDown = False
 				if movUp == True:
 					lead.y_change = -WALKING_SPEED
-		
-		
+
+
 	collide = wallCollision(lead)
 	current_Room = roomChange(lead, current_Room)
-	
+
 	if collide == False:
 		lead.x += lead.x_change
-		lead.y += lead.y_change		
+		lead.y += lead.y_change
 		drawLead(lead)
 	if collide == True:
 		drawLead(lead)
-	
-	
+
+
 	pygame.display.update()
 	clock.tick(30)
 pygame.quit()
